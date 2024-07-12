a39d3ae0a2c6b93ce97ee2ca2f7c4775d93ed85d
Mykhailo Kuzich <timmkedth@gmail.com>
2024-07-12 21:36:39 +0200

    completed

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,12 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('.hero-cta'),
-    closeModalBtn: document.querySelector('.modal-close'),
+    closeModalBtn: document.querySelector('#modal-close'),
     modal: document.querySelector('.modal-overlay'),
+    openMenuBtn: document.querySelector('.header-menu-btn'),
+    closeMenuBtn: document.querySelector('#menu-close'),
+    menu: document.querySelector('.modal-menu'),
+    body: document.querySelector('body'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -10,5 +14,14 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
+    refs.body.classList.toggle('no-scroll');
+  }
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
